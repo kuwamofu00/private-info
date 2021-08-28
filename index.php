b8771c218de43dde83d5395d08982544de97c035
kuwamofu00 <88776655443322wot@gmail.com>
2021-08-28 11:21:38 +0900

    fi

--- index.php
+++ index.php
@@ -60,77 +60,16 @@
   </header>
 
   <main class="px-3">
-    <h1>SUMIF VBA source</h1>
+    <h1>LG22インチディスプレイ　動作確認</h1>
     <p class="lead font-grey">
 
-      ......................................................................................................<br>
-      パターン１ 差分があるファイルがわかっているが差分の内容が分からないとき<br>
-      ......................................................................................................<br>
-      ⭕index1.phpとindex2.phpの差分を見る<br>
-<br>
-      kuwamofu@kuwamofu-H81M-D3V-JP:/$ pwd<br>
-        /<br>
-<br>
-      kuwamofu@kuwamofu-H81M-D3V-JP:~/laravel-project$ pwd<br>
-        /home/kuwamofu/laravel-project<br>
-<br>
-      kuwamofu@kuwamofu-H81M-D3V-JP:~/laravel-project$ ls<br>
-        README.md  artisan    composer.json  config    index1.php  package.json  public     routes      storage  vendor<br>
-        app        bootstrap  composer.lock  database  index2.php  phpunit.xml   resources  server.php  tests    webpack.mix.js<br>
-<br>
-      kuwamofu@kuwamofu-H81M-D3V-JP:~/laravel-project$ diff index1.php index2.php <br>
-        7c7<br>
-        < <br>
-        ---<br>
-        > koko<br>
-        11c11<br>
-        < <br>
-        ---<br>
-        > koko<br>
-        ７行目と11行目にkokoという文字列が差分である事がわかる<br>
-<br>
-      ⭕ファイルを並べて表示してみる　index2.phpの７行目と11行目に差分有り<br>
-        kuwamofu@kuwamofu-H81M-D3V-JP:~/memo$ diff -y index1.php index2.php<br>
+      <div>
+        <span><img src="IMG20210828104738.jpg" alt="" class="img-fluid"></span>
+        <span><img src="IMG20210828104738.jpg" alt="" class="img-fluid"></span>
+      </div>
 
-        !DOCTYPE html							                                      !DOCTYPE html<br>
-        html lang="ja" class="h-100" head				                      html lang="ja" class="h-100" head<br>
-        meta charset="utf-8"					                                  meta charset="utf-8"<br>
-            meta name="viewport" content="width=device-width, initia	      meta name="viewport" content="width=device-width, initia<br>
-            meta name="description" content="本当においしいものを生	           meta name="description" content="本当においしいものを生<br>
-            title test /title					                                title test /title<br>
-        							                                                   |	koko<br>
-            !-- Bootstrap core CSS -->					                              <!-- Bootstrap core CSS --><br>
-            link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-	        link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-<br>
-            link rel="stylesheet" href="index.css"			                     link rel="stylesheet" href="index.css"<br>
-        							                                                   |	koko<br>
-        style							                                                   style <br>
-              .bd-placeholder-img {					                                        .bd-placeholder-img {<br>
-<br>
-      ......................................................................................................<br>
-      パターン２ 差分のある箇所がわかるが差分のあるファイルを特定を特定できないとき<br>
-      ......................................................................................................<br>
-      ⭕例　'koko'という文字列が含まれているファイルを特定する<br>
-      検索対象ファイルがあるであろうディレクトリに移動する<br>
-      ここではuserのlaravel-project に移動して検索してみる<br>
-<br>
-      kuwamofu@kuwamofu-H81M-D3V-JP:~/laravel-project$ grep koko /home/kuwamofu/laravel-project<br>
-        grep: /home/kuwamofu/laravel-project/app: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/bootstrap: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/config: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/database: ディレクトリです<br>
-        /home/kuwamofu/laravel-project/index2.php:koko<br>
-        /home/kuwamofu/laravel-project/index2.php:koko<br>
-        grep: /home/kuwamofu/laravel-project/public: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/resources: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/routes: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/storage: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/tests: ディレクトリです<br>
-        grep: /home/kuwamofu/laravel-project/vendor: ディレクトリです<br>
-<br>
-      kokoを含むファイルを発見　index2.phpであることがわかりました。<br>
-<br>
-      以上<br>
-</p>
+
+
     <p>
     <a class="btn2 btn-lg btn-secondary-2 fw-bold" ></a>
       <div class="font-grey">
